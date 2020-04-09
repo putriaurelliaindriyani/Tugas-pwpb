45ab71aae67cb8fdb5254ee51140ac52239e5d08
putriaurelliaindriyani <63333616+putriaurelliaindriyani@users.noreply.github.com>
2020-04-09 07:54:42 +0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php 
+echo "Nama : Putri Aurellia Indriyani";
+echo "Kelas : XI RPL 2";
+echo "Asal Sekolah : SMK TARUNA BHAKTI DEPOK";
+echo "Alamat : Kp.sidamukti RT07/07 no 73 jawa barat,kota depok, kota depok";
+?>
